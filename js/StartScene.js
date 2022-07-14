4f6fa4217ebed264c7eea51d5df8a57f0c9d26bd
Josh Rahn <jprahn01@gmail.com>
2022-07-14 14:47:53 -0400

    Update StartScene.js

--- js/StartScene.js
+++ js/StartScene.js
@@ -12,7 +12,7 @@ class StartScene extends Phaser.Scene{
         //gameState.cursors = this.input.keyboard.createCursorKeys();
         
         // Add start screen text and prompt
-        this.add.text((config.width/2), (config.height/2), "PUNG: The Game of Champiungs\n\nHit Enter to Play\n\nor don't...I don't care", {align: 'center'}).setOrigin(0.5);
+        this.add.text((config.width/2), (config.height/2), "PUNG: The Game of Champiungs\n\nClick to Play", {align: 'center'}).setOrigin(0.5);
 
         this.input.on('pointerup', () => {
             this.scene.stop('StartScene');
